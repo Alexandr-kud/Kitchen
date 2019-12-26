3a65aab0a169c22cfa9797e72878a19c0ca7cc3c
Alexandr Kudryavtsev <alexandr.ink@yandex.ru>
2019-12-26 23:06:53 +0300

    Главная страница готова на 80%

--- js/main.js
+++ js/main.js
@@ -57,4 +57,47 @@ var swiper = new Swiper('.swiper-container', {
   });
 
 
+  $('.hamburger').click(function() {
+    $('.menu-collapse').toggleClass('nav-none');
+    $('.nav__list').toggleClass('nav__list_open');
+    $('.nav__list_menu-hiden_main').toggleClass('d-block');
+    $('.hamburger__item').toggleClass('hamburger__item_open');
+  });
+  
+  $('.sub > a').click(function(){
+    $('.sub ul').slideUp();
+      if ($(this).next().is(":visible")){
+          $(this).next().slideUp();
+      } else {
+        $(this).next().slideToggle();
+      }
+    return false;
+  });
 
+    $('.menu-collapse > ul > li > a').click(function(){
+      $('.menu-collapse > ul > li > a, .sub a').removeClass('active');
+      $(this).addClass('active');
+      }),
+      $('.sub ul li a').click(function(){
+        $('.sub ul li a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.sub1 > a').click(function(){
+        $('.sub1 ul').slideUp();
+        if ($(this).next().is(":visible")){
+            $(this).next().slideUp();
+        } else {
+          $(this).next().slideToggle();
+        }
+      return false;
+    });
+
+    $('.menu-collapse > ul > li > a').click(function(){
+      $('.menu-collapse > ul > li > a, .sub1 a').removeClass('active');
+      $(this).addClass('active');
+      }),
+      $('.sub1 ul li a').click(function(){
+        $('.sub1 ul li a').removeClass('active');
+        $(this).addClass('active');
+    });
