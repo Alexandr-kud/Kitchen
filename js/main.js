e7795626ac17d268f2eb5973563b4e6fe59b993c
Alexandr Kudryavtsev <alexandr.ink@yandex.ru>
2019-12-26 15:00:00 +0300

    Подключение свип слайдера

--- js/main.js
+++ js/main.js
@@ -33,8 +33,21 @@ $(document).ready(function () {
             }
         ]
     });
+
+    
+
 });
 
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
 
 
